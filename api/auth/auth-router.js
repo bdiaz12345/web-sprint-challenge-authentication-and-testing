2e9e0f2be933223896692331e2ac4cb8e578c2b3
bdiaz12345 <bdiazmusic5@gmail.com>
2021-02-26 15:32:25 -0500

    mvp

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const db = require('../../data/dbConfig')
+
+const bcryptjs = require("bcryptjs");
+const makeToken = require('../middleware/makeToken')
 
 router.post('/register', (req, res) => {
-  res.end('implement register, please!');
+  const user = req.body
+  if (isValid(user)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    // hash the password
+    const hash = bcryptjs.hashSync(user.password, rounds);
+
+    user.password = hash;
+
+    // save the user to the database
+    return db('users').insert(user)
+      .then(user => {
+        res.status(201).json({ data: user });
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
+  function isValid(user) {
+    return Boolean(user.username && user.password && typeof user.password === "string");
+  }
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
@@ -28,8 +56,35 @@ router.post('/register', (req, res) => {
   */
 });
 
-router.post('/login', (req, res) => {
-  res.end('implement login, please!');
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  if (isValid(req.body)) {
+    await db('users').where('username', username)
+        .then(([user]) => {
+          // compare the password the hash stored in the database
+          if (user && bcryptjs.compareSync(password, user.password)) {
+            const token = makeToken(user)
+
+            res.status(200).json({ 
+              message: "Welcome to our API " + user.username,
+              token            
+            });
+          } else {
+            res.status(401).json({ message: "Invalid credentials" });
+          }
+        })
+        .catch(error => {
+          res.status(500).json({ message: error.message });
+        });
+  } else {
+    res.status(400).json({
+      message: "please provide username and password and the password shoud be alphanumeric",
+    });
+  }
+  function isValid(user) {
+    return Boolean(user.username && user.password && typeof user.password === "string");
+  }
+
   /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,47 @@
 // Write your tests here
+const request = require("supertest")
+const db = require("../data/dbConfig.js")
+const server = require("./server.js")
+
+const brandon = {username: "Brandon", password: 'Garcia'}
+
+beforeAll(async ()=>{
+    await db.migrate.rollback()
+    await db.migrate.latest()
+})
+
+beforeEach(async ()=>{
+    await db("users").truncate()
+})
+
+afterAll(async ()=>{
+    await db.destroy()
+})
+
+describe("server", ()=>{
+    describe("[GET] /jokes", ()=>{
+        it("resopnds with 403 status if not user", async ()=>{
+            const res = await request(server).get("/api/jokes")
+            expect(res.status).toBe(403)
+        })
+    })
+    describe("[POST] /users", ()=>{
+        it("responds with  newly created user", async ()=>{
+            let res
+            res = await request(server).post("/api/auth/register").send(brandon)
+            console.log(res.status)
+            expect(res.status).toEqual(201)
+        })
+      })
+      describe('[POST] /login', () => {
+        it('responds with good status', async () => {
+          await request(server).post('/api/auth/register').send(brandon)
+          let res
+          res = await request(server).post('/api/auth/login').send(brandon)
+          expect(res.status).toEqual(200)
+        })
+    })
+})
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
